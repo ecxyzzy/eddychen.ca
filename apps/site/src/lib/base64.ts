27426a3f530ca6716c5a0792319b79c09cbaedf2
Eddy Chen <e@eddychen.ca>
2026-05-09 01:12:20 -0700

    swapped paren

--- apps/site/src/lib/base64.ts
+++ apps/site/src/lib/base64.ts
@@ -4,7 +4,7 @@ export const base64UrlToBase64 = (str: string): Uint8Array =>
   Maybe.some(str.replace(/-/g, "+").replace(/_/g, "/"))
     .map((s) => s + "=".repeat((4 - (s.length % 4)) % 4))
     .map(atob)
-    .map((s) => new Uint8Array(Array.from(new Array(s.length).map((_, i) => s.charCodeAt(i)))))
+    .map((s) => new Uint8Array(Array.from(new Array(s.length)).map((_, i) => s.charCodeAt(i))))
     .unwrap();
 
 export const base64Encode = (str: string): Uint8Array => new TextEncoder().encode(str);
